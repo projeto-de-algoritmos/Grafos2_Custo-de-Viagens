6671a93624a809ccaae4acb1a86384e8a5defa4b
luucas-melo <lucasmelodos322@gmail.com>
2022-02-18 21:30:56 -0300

    Add navbar component

---
+++ custo_viagens/src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import { Flex } from 'reflexbox';
+import * as S from './styles';
+const Navbar = () => {
+  return (
+    <S.NavContainer>
+      <h2>Custo de viagem</h2>
+    </S.NavContainer>
+  );
+};
+
+export default Navbar;
---
+++ custo_viagens/src/components/Navbar/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const NavContainer = styled.div`
+  display: flex;
+  height: 50px;
+  background: #10add4;
+  color: white;
+  align-items: center;
+  padding: 0.3rem;
+  margin-bottom: 2rem;
+`;
